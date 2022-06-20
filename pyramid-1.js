da59b301621929c98fb802c330f27592acb4e967
Thayna Rodrigues <thna.rdg@gmail.com>
2022-06-20 01:24:27 -0300

    Add comments

--- pyramid-1.js
+++ pyramid-1.js
@@ -1,5 +1,15 @@
+/* 
+PYRAMID 1 -> Use loops to print a half pyramid to the console like:
+    #
+   ##
+  ### 
+ ####
+#####
+*/
+
 function makePyramid(num) { 
-    if (num > 1 && num < 10) {
+    // Limit the height so the max possible height is 9 and the min is 3.
+    if (num > 2 && num < 10) {
         let height = num;
         for (let row = 0; row < height; row++) {
             for (let i = row + 1; i < height; i++) {
@@ -11,7 +21,7 @@ function makePyramid(num) {
             console.log("");
         }
     } else {
-        console.log("Not valid");
+        console.log("Not valid. The parameter should be an interger between 3 and 9");
     }
 }
 makePyramid(8);
